c938578ccca9cfe2136870e5e480e42b21eb7691
Michael JasonSmith <mpj17@onlinegroups.net>
2013-01-30 16:57:39 +1100

    s/minor/muted/ for Bootstrap

--- gs/content/js/multifile/browser/javascript/gsfileupload.js
+++ gs/content/js/multifile/browser/javascript/gsfileupload.js
@@ -17,7 +17,7 @@ GSFileUpload = function () {
         return true;
       };
     var addMultiFile = function () {
-        var remove = '<abbr class="minor" title="Remove this file from the list of files">(remove)</abbr>';
+        var remove = '<abbr class="muted" title="Remove this file from the list of files">(remove)</abbr>';
         var options = {
           list: '#fileList',
           STRING: {remove: remove}
